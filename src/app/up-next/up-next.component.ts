167739dfd7a5c49275fe2eb4624beea6358c39fe
maeganwilson_ <maegan@maeganwilson.com>
2018-09-24 08:42:26 -0500

    Adding upnext component

---
+++ src/app/up-next/up-next.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "up-next",
+  templateUrl: "./up-next.component.html",
+  styleUrls: ["./up-next.component.css"]
+})
+export class UpNextComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
